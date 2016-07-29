1a2b57574c24e44c044fff1430ad929146713f8f
dhs-biyu <chou.biyu@dhs.sg>
2016-07-29 16:30:12 +0800

    Revert "Changes .-."
    This reverts commit

--- app.js
+++ app.js
@@ -1,69 +1,18 @@
-var index = 0;
-
 $(function() {
-    $("#setup, #punchline, #voting, #votes-container").hide();
+// code for clicking button .etc
 
     $("#getJoke").click(
         function() {
-            $("#setup, #punchline, #voting, #votes-container").show();
-
             $.get("/jokes",function(data){
-                $("#setup").html(data.setup);
-                $("#punchline").html(data.punchline);
-
-                index = data.id;
-
-                if (data.votes === undefined) {
-                    $("#votes").html(0);
-                } else {
-                    $("#votes").html(data.votes);
-                }
-
-                changeVoteColor(data.votes);
-
+                // TODO:
+                // modify this code to manipulate the HTML
+                // to display the setup and punchline on the page
+                // instead of using alerts!
+               //alert(data.setup);
+               //alert(data.punchline);
+              $("#setup").html(data.setup);
+              $("#punchline").html(data.punchline);
             },"json")
         }
     );
-
-    $("#upvote").on("click", function() {
-        // $.post("/upvote");
-        $.ajax({
-            url: '/upvote',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify({ id: index }),
-
-            success: function(data, status, xhr) {
-                $("#votes").html(data.votes);
-
-                changeVoteColor(data.votes);
-            }
-        });
-    });
-
-    $("#downvote").on("click", function() {
-        // $.post("/downvote");
-        $.ajax({
-            url: '/downvote',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify({ id: index }),
-
-            success: function(data, status, xhr) {
-                $("#votes").html(data.votes);
-
-                changeVoteColor(data.votes);
-            }
-        });
-    });
-
-    function changeVoteColor(numVotes) {
-        if (numVotes < 0) {
-            $("#votes").css('color', 'red');
-        } else if (numVotes > 0) {
-            $("#votes").css('color', 'lightgreen');
-        } else {
-            $("#votes").css('color', 'white');
-        }
-    };
 });
