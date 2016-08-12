7a0573dbcf911186a943ff33143d33fad044c125
dhs-biyu <chou.biyu@dhs.sg>
2016-08-12 17:12:35 +0800

    able to update jokes:)

--- app.js
+++ app.js
@@ -1,41 +1,128 @@
 var index = 0;
- 
+
 $(function() {
+    $("#setup, #punchline, #voting, #votes-container").hide();
+    refreshJokes();
+
     $("#getJoke").click(
         function() {
+            $("#setup, #punchline, #voting, #votes-container").show();
+
             $.get("/jokes",function(data){
                 $("#setup").html(data.setup);
                 $("#punchline").html(data.punchline);
+
                 index = data._id;
-                alert(index);
-                //alert(index);
+
+                if (data.votes === undefined) {
+                    $("#votes").html(0);
+                } else {
+                    $("#votes").html(data.votes);
+                }
+
+                changeVoteColor(data.votes);
+
             },"json")
         }
     );
-   
-    $("#upvote").on('click', function() {
-        //$.post('/upvote');
+
+    $("#upvote").on("click", function() {
         $.ajax({
             url: '/upvote',
-            type: 'POST',
+            type: 'PUT',
             contentType: 'application/json',
             data: JSON.stringify({ id: index }),
+
             success: function(data, status, xhr) {
-                $('#votes').html(data.votes);
+                $("#votes").html(data.votes);
+
+                changeVoteColor(data.votes);
             }
         });
     });
-   
-    $("#downvote").on('click', function() {
-        // $.post('/downvote');
+
+    $("#downvote").on("click", function() {
         $.ajax({
             url: '/downvote',
-            type: 'POST',
+            type: 'PUT',
             contentType: 'application/json',
             data: JSON.stringify({ id: index }),
-                success: function(data, status, xhr) {
-                    $('#votes').html(data.votes);
-                }
+
+            success: function(data, status, xhr) {
+                $("#votes").html(data.votes);
+
+                changeVoteColor(data.votes);
+            }
         });
     });
+
+    $("#createJoke").submit(function(event) {
+        event.preventDefault();
+
+        var setup = $('#createJokeSetup').val();
+        var punchline = $('#createJokePunchline').val();
+
+        $.ajax({
+            url: '/createjoke',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(
+                {
+                    "setup": setup,
+                    "punchline": punchline,
+                    "votes": 0
+                }
+            ),
+            success: function(data, status, xhr) {
+                $('#createJokeSetup').val("");
+                $('#createJokePunchline').val("");
+                $('#createJokeSuccess').text("Joke created successfully. {setup: " + setup + ", punchline: " + punchline + "}");
+                refreshJokes();
+            }
+        });
+    });
+
+    $("#refreshJokes").on('click', function() {
+        refreshJokes();
+    });
+
+    $("#deleteJoke").on('click', function() {
+       $.ajax({
+           url: 'deletejoke',
+           type: 'DELETE',
+           contentType: 'application/json',
+           data: JSON.stringify(
+               {
+                    id: $('#allJokes').val()
+               }
+           ),
+            success: function(data, status, xhr) {
+                $('#deleteJokeSuccess').text("Joke deleted successfully. {setup: " + $('#allJokes option:selected').text() + "}");
+                refreshJokes();
+            }
+       })
+    });
+
+    function changeVoteColor(numVotes) {
+        if (numVotes < 0) {
+            $("#votes").css('color', 'red');
+        } else if (numVotes > 0) {
+            $("#votes").css('color', 'lightgreen');
+        } else {
+            $("#votes").css('color', 'white');
+        }
+    }
+
+    function refreshJokes() {
+        $.get("/alljokes", function(data) {
+            var optionsHTML = "";
+            $('#allJokes').empty();
+            data.forEach(function(element, index, array) {
+                $('#allJokes')
+                    .append($("<option></option>")
+                    .attr("value",element._id)
+                    .text(element.setup + " " + element.punchline));
+            });
+        });
+    }
 });
--- server.js
+++ server.js
@@ -4,16 +4,23 @@ var mongodb = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
 var app = express();
 var port = 8888;
- 
+var db, collection;
+
+var uri = "mongodb://" + process.env.MONGOLAB_USER + ":" + process.env.MONGOLAB_PASSWORD + "@ds153745.mlab.com:53745/nodewebdev";
 var jokes=[{setup:"Our wedding was so beautiful,",punchline:"even the cake was in tiers", votes: 0},{setup:"I'm reading a book on the history of glue",punchline:"I just can't seem to put it down", votes: 0},{setup:"What do you call an Argentinian with a rubber toe?",punchline:"Roberto", votes: 0}];
-var uri = "mongodb://anonymousby1:weakpassword1@ds023485.mlab.com:23485/webdev";
 
 app.use(bodyParser.json());
-app.listen(process.env.PORT || port, function(){
-    console.log("Listening on " + port);
+app.use(express.static('static'));
+
+mongodb.connect(uri, function(err, database) {
+   db = database;
+   collection = db.collection('jokes');
+
+    app.listen(process.env.PORT || port, function() {
+        console.log("Listening on " + port);
+    });
 });
 
-app.use(express.static('static'));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -27,69 +34,86 @@ app.get('/style.css', function(req, res) {
 	res.sendFile(__dirname + '/style.css');
 });
 
-app.post('/upvote', function(req, res) {
-    console.log("someone upvoted.");
-    console.log(req.body);
-   
-    var jokeIndex = req.body.id;
-   
-    mongodb.connect(uri, function(err, db) {
-        var collection = db.collection('jokes');
-        collection.findOneAndUpdate( {_id: ObjectId(jokeIndex)}, { $inc: {"votes": 1} }, function(err, result) {
-            console.log(result.value);
-            result.value.votes++;
-            res.send(result.value);
-        });
-    });
-   
-});
- 
-app.post('/downvote', function(req, res) {
-    console.log("someone downvoted.");
-    console.log(req.body);
-   
-    var jokeIndex = req.body.id;
-   
-    mongodb.connect(uri, function(err, db) {
-        var collection = db.collection('jokes');
-        collection.findOneAndUpdate( {_id: ObjectId(jokeIndex)}, { $inc: {"votes": -1} }, function(err, result) {
-            console.log(result.value);
-            result.value.votes--;
-            res.send(result.value);
-        });
-    });
-   
-});
- 
- 
 app.get('/jokes', function(req, res) {
- 
-    mongodb.connect(uri, function(err, db) {
-        var collection = db.collection('jokes');
-        collection.count(function(err, count) {
-            var randomNumber = Math.floor(Math.random() * count);
-            console.log(count + "jokes found in database.");
-           
-            collection.find().limit(-1).skip(randomNumber).next(
-                function(err, results) {
-                    console.log(results);
-                    res.send(results);
-                }
-            );
-        });
-    });
-});
- 
-app.get('/admin', function(req, res) {
-   
-    console.log('Attempting to add into database.');
-    mongodb.connect(uri, function(err, db) {
-        var collection = db.collection('jokes');
-        collection.insertMany(jokes,
+
+    collection.count(function(err, count) {
+
+        var randomNumber = Math.floor(Math.random() * count);
+
+        collection.find().limit(-1).skip(randomNumber).next(
             function(err, results) {
                 res.send(results);
-                db.close();
             }
         );
     });
+});
+
+app.put('/upvote', function(req, res) {
+    var jokeIndex = req.body.id;
+    collection.findOneAndUpdate(
+        { _id: ObjectId(jokeIndex) },
+        { $inc: { "votes" : 1 }},
+        function(err, result) {
+            result.value.votes++;
+            res.send(result.value);
+        }
+    );
+});
+
+app.put('/downvote', function(req, res) {
+    var jokeIndex = req.body.id;
+    collection.findOneAndUpdate(
+        { _id: ObjectId(jokeIndex) },
+        { $inc: { "votes" : -1 }},
+        { returnNewDocument: true },
+        function(err, result) {
+            result.value.votes--;
+            res.send(result.value);
+        }
+    );
+});
+
+/*
+app.get('/admin', function(req, res) {
+
+     console.log('Attempting to add into database.');
+     mongodb.connect(uri, function(err, db) {
+         var collection = db.collection('jokes');
+        collection.insertMany(jokes,
+             function(err, results){
+                 res.send(results);
+                 db.close();
+            }
+         );
+     });
+ });*/
+
+app.get('/updatejokes', function(req, res) {
+
+    res.sendFile(__dirname + '/updateJokes.html');
+});
+
+app.post('/createjoke', function(req, res) {
+
+    collection.insertOne(req.body,
+        function(err, results){
+            res.send(results);
+        }
+    );
+});
+
+app.get('/alljokes', function(req, res) {
+    collection.find().toArray(
+        function(err, resultArray) {
+            res.send(resultArray)
+        }
+    );
+});
+
+app.delete('/deletejoke', function(req, res) {
+    collection.deleteOne({_id: ObjectId(req.body.id)},
+        function(err, results){
+            res.send(results);
+        }
+    );
 });
